c49d7e8828761b7e2bb84149f9cd1f2ae3470227
JiHoonAHN <ahnjh1028@naver.com>
2022-10-04 17:21:05 +0900

    Make Project

---
+++ Tests/JiHoonSwiftExtTests/JiHoonSwiftExtTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import JiHoonSwiftExt
+
+final class JiHoonSwiftExtTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(JiHoonSwiftExt().text, "Hello, World!")
+    }
+}
